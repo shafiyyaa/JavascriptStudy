005bf17fc3ea4149425171f5e42a59787f07b543
Wan Shafiyya Lazuardi <shafiyyalazuardi@gmail.com>
2024-06-08 16:01:26 +0700

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,11 @@
+// Tentukan total jumlah dari array berikut:
+// Var scores = [1,2,3,4,5];
+
+var scores = [1, 2, 3, 4, 5];
+var total = 0;
+
+for (var i = 0; i < scores.length; i++) {
+    total += scores[i];
+}
+
+console.log(total);
