88499199b67d7b5472ba5b6f305795643052f2d5
Wan Shafiyya Lazuardi <shafiyyalazuardi@gmail.com>
2024-06-08 15:59:40 +0700

    Add files via upload

---
+++ Kondisi_Bersarang.js
@@ -0,0 +1,42 @@
+var number = 10;
+var str = "buku";
+var isCorrect = true;
+
+// console.log(number);
+// console.log(str);
+// console.log(isCorrect);S
+
+//IF ELSE
+if (umur => 17) {
+    console.log("Bisa bikin KTP")
+} else {
+    console.log("Belum bisa bikin KTP")
+}
+
+//SWITCH CASE
+var pilih_minuman = "kopi"
+switch (pilih_minuman) {
+    case"kopi":
+        console.log("Pilihan minuman kopi");
+        break;
+    case"teh":
+        console.log("Pilihan minuman teh");
+        break;
+    default:
+        console.log("Pilihan minuman tidak ada")
+        break;
+}
+
+//IF ELSE
+var umur = 16
+var tinggi = 145
+
+if (umur < 17) {
+    if (tinggi < 150) {
+        console.log("Tidak Boleh Masuk")
+    } else {
+        console.log("Boleh Masuk")
+    }
+} else {
+    console.log("Boleh Masuk")
+}
